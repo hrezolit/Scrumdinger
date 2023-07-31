18d112f80ae7964faea677d1b4a3039299ca181d
Nik <hrezolitaz@gmail.com>
2023-07-31 18:06:16 +0600

    Add scrum history

---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Nikita on 31/7/23.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
+}
