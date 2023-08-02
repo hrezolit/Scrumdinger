12892d5a63515636ffd0df820bec789bb544391d
Nik <hrezolitaz@gmail.com>
2023-08-02 09:38:49 +0600

    load data on app when it's launch

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -10,11 +10,18 @@ import SwiftUI
 @main
 struct ScrumdingerApp: App {
     
-    @State private var scrums = DailyScrum.exampleData
+    @StateObject private var store = ScrumStore()
     
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: $scrums)
+            ScrumsView(scrums: $store.scrums)
+                .task {
+                    do {
+                        try await store.load()
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
         }
     }
 }
